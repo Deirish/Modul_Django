e9b8f72e96a08b6419a7f2f3272f248c37e390db
Irina <lykin4ik@gmail.com>
2023-02-07 18:09:56 +0200

    models

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    cash = models.DecimalField(max_digits=7, decimal_places=2, blank=True, null=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+    image = models.ImageField(upload_to='product/%Y/%m/%d', blank=True)
+    description = models.TextField(max_length=1000, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.PositiveSmallIntegerField()
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+    def __str__(self):
+        return self.name
+
+
+class Purchase(models.Model):
+    client = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    available = models.PositiveSmallIntegerField()
+    purchase_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-purchase_date']
+        verbose_name = 'Purchase'
+        verbose_name_plural = 'Purchases'
+
+    def __str__(self):
+        return self.product
+
+    def total(self):
+        return self.product.price * self.available
+
+
+class Return(models.Model):
+    purchase = models.OneToOneField(Purchase, on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date']
+
+    def __str__(self):
+        return self.purchase
--- myshop/settings.py
+++ myshop/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,7 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+AUTH_USER_MODEL = 'myapp.User'
 # Application definition
 
 INSTALLED_APPS = [
@@ -76,12 +77,14 @@ WSGI_APPLICATION = 'myshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'myshop',
-        'USER': 'diren',
-        'PASSWORD': 'nazi2812',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': 'myshop',
+        # 'USER': 'diren',
+        # 'PASSWORD': 'nazi2812',
+        # 'HOST': 'localhost',
+        # 'PORT': '5432',
     }
 }
 
@@ -121,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
